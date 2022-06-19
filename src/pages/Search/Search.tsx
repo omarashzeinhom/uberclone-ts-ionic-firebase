0df71c0d7c9564f0e204c7208cd013a023376ab0
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-19 10:51:11 +0200

    added-gui-for-search-page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,6 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer/ExploreContainer';
-import './Search.scss';
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../../components/ExploreContainer/ExploreContainer";
+import "./Search.scss";
 
 const Search: React.FC = () => {
   return (
@@ -8,10 +20,25 @@ const Search: React.FC = () => {
       <IonHeader>
         <IonToolbar>
           <IonTitle>Search</IonTitle>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="home" />
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        
+        <IonGrid>
+          <IonRow>
+            <IonInput placeholder="Where" />
+          </IonRow>
+          <IonRow>
+            <IonInput placeholder="Go to" />
+          </IonRow>
+          <IonRow>
+            <IonButton expand="block" color={"dark"}>
+              Search
+            </IonButton>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
