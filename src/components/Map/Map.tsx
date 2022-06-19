1c5999c30af1909a07d14b997b94c7d3bd0716cc
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-20 01:05:16 +0200

    added-confirm-tab

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,44 +1,50 @@
 import React from "react";
 //scss
-import './Map.scss';
+import "./Map.scss";
 import { useEffect } from "react";
 
-const mapboxgl = require('mapbox-gl');
+const mapboxgl = require("mapbox-gl");
 
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g';
-
-
-
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g";
 
 interface MapsProps {
-    name: string;
+  name: string;
+}
 
-  }
-  
-  const Maps: React.FC<MapsProps> = ({ name }) => {
+const Map: React.FC<MapsProps> = ({ name }) => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      //important id for the container insert into div
+      container: "map",
+      style:
+        "mapbox://styles/omarashzeinhom98/cl4k5xuzh002h14mtoho5qips?optimize=true",
+      center: [26.8206, 30.8025],
+      zoom: 3,
+    });
+  });
 
-
-    useEffect(() => {
-      const map = new mapboxgl.Map({
-        //important id for the container insert into div
-        container: "map",
-        style: 'mapbox://styles/omarashzeinhom98/cl4k5xuzh002h14mtoho5qips?optimize=true',
-        center: [26.8206, 30.8025],
-        zoom: 3,
-        
+const addToMap = (map: any) => {
+      // Set marker options.
+      const marker = new mapboxgl.Marker({
+        color: "#568203",
+        draggable: true,
       })
-    
-   
-      });
+        .setLngLat([26.8206, 30.8025])
+        .addTo(map);
+}
 
 
-    return (
-      <div className="container__map" /*important id for map container */id="map">
-        <strong>{name}</strong>
-      </div>
-    );
-  };
-  
-  export default Maps;
-  
+
+
+  return (
+    <div
+      className="container__map"
+      /*important id for map container */ id="map"
+    >
+      <strong>{name}</strong>
+    </div>
+  );
+};
+
+export default Map;
--- src/pages/ConfirmTab/ConfirmTab.tsx
+++ src/pages/ConfirmTab/ConfirmTab.tsx
@@ -1,8 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './ConfirmTab.scss';
-import Map from '../../components/Map/Map';
+import {
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./ConfirmTab.scss";
+import Map from "../../components/Map/Map";
+import { useEffect } from "react";
 
 const ConfirmTab: React.FC = () => {
+  const getCoordiantes = () => {
+    const location = "Cairo";
+    // Fetch Function
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  //call function with useEffect
+
+  useEffect(() => {
+    getCoordiantes();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +42,12 @@ const ConfirmTab: React.FC = () => {
             <IonTitle size="large">Confirm</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <Map name={'map'}/>
+        <Map name={"map"} />
+        <IonGrid>
+          {/**Worker Selector */}
+
+          {/**Confirm Button */}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
