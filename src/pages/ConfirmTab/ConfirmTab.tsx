d4b4b2ade499d607fe6b6d75e14f054d09c82e68
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-21 20:48:11 +0200

    added-useRouter-custom-for-type-script

--- src/pages/ConfirmTab/ConfirmTab.tsx
+++ src/pages/ConfirmTab/ConfirmTab.tsx
@@ -22,11 +22,10 @@ interface MapsProps {
 
 const ConfirmTab: React.FC<MapsProps> = (props, { pickup, dropoff }) => {
   const router = useRouter();
-//debug router 
-console.log(router);
+  //debug router
+  console.log(router);
   //debug props
   //console.log(props);
-
 
   const [pickupCoordinates, setPickupCoordinates] = useState("");
   const [dropOffCoordinates, setDropOffCoordinates] = useState("");
