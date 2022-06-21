65ed363ddea7e11f43278c7da9ef4d0fa9d6ac14
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-22 01:31:22 +0200

    adding-gui-for-confirm-container

--- src/components/WorkerSelector/WorkerSelector.tsx
+++ src/components/WorkerSelector/WorkerSelector.tsx
@@ -1,11 +1,16 @@
 import {
+  IonAvatar,
   IonContent,
+  IonImg,
   IonItem,
+  IonItemDivider,
   IonLabel,
   IonList,
   IonPage,
+  IonRouterLink,
   IonSelect,
   IonSelectOption,
+  IonText,
 } from "@ionic/react";
 import React from "react";
 import "./WorkerSelector.scss";
@@ -13,10 +18,33 @@ import "./WorkerSelector.scss";
 const WorkerSelector: React.FC = () => {
   return (
     <IonList>
+      <IonRouterLink>
+        <IonItem>
+          <IonAvatar slot="start">
+            <IonImg src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655851971/FIXITAPP/ionic-app-images/WorkerSelector/electrician-svgrepo-com_w1mh7o.svg" />
+          </IonAvatar>
+          <IonText slot="start">
+            <p>Electrician</p>
+            <small color={"success"}>15 mins away</small>
+          </IonText>
+
+          <IonText slot="end">
+            <p> Price $ </p>
+          </IonText>
+        </IonItem>
+      </IonRouterLink>
+
+      <IonItemDivider />
+
       <IonItem>
         <IonLabel>Workers available</IonLabel>
         <IonSelect>
-          <IonSelectOption value={"Plumber"}>Plumber</IonSelectOption>
+          <IonSelectOption value={"Plumber"}>
+            <IonText>Plumber</IonText>
+            <IonAvatar>
+              <IonImg src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655851971/FIXITAPP/ionic-app-images/WorkerSelector/electrician-svgrepo-com_w1mh7o.svg" />
+            </IonAvatar>
+          </IonSelectOption>
           <IonSelectOption value={"Carpenter"}>Carpenter</IonSelectOption>
           <IonSelectOption value={"Electrician"}>Electrician</IonSelectOption>
           <IonSelectOption value={"Cable"}>Cable</IonSelectOption>
