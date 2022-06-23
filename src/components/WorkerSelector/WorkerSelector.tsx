50fbd7d6f5ea0f012e9ae2b5b945d58b0c9b83cd
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-23 23:55:18 +0200

    fixed-dropoff-and-pickupcoordinates-error-in-selector-by-addingthem-in-an-array-got-idea-from-stack-overflow

--- src/components/WorkerSelector/WorkerSelector.tsx
+++ src/components/WorkerSelector/WorkerSelector.tsx
@@ -16,8 +16,11 @@ interface MapsProps {
   pickupCoordinates: string;
 }
 
-const WorkerSelector: React.FC<MapsProps> = ({pickupCoordinates,dropOffCoordinates}) => {
-  // Make Sure Maps props are passed here	
+const WorkerSelector: React.FC<MapsProps> = ({
+  pickupCoordinates,
+  dropOffCoordinates,
+}) => {
+  // Make Sure Maps props are passed here
   //console.log(pickupCoordinates,dropOffCoordinates)
   const [tripTime, setTripTime] = useState(0);
 
@@ -25,12 +28,16 @@ const WorkerSelector: React.FC<MapsProps> = ({pickupCoordinates,dropOffCoordinat
     //get trip time from map box api
     //2pickup coordinates and 2 points for the dropoff location [x]
     fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/driving/${[pickupCoordinates][0]},${[pickupCoordinates][1]};${[dropOffCoordinates][0]},${[dropOffCoordinates][1]}?access_token=pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g`
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${
+        [pickupCoordinates][0]
+      },${[pickupCoordinates][1]};${[dropOffCoordinates][0]},${
+        [dropOffCoordinates][1]
+      }?access_token=pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g`
     );
-  }, [dropOffCoordinates,pickupCoordinates]);
+  }, [dropOffCoordinates, pickupCoordinates]);
 
   const htt = `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropOffCoordinates[0]},${dropOffCoordinates[1]}?access_token=pk.eyJ1Ijoib21hcmFzaHplaW5ob205OCIsImEiOiJjbDRrMXY5c3MwN3ZpM2NxcHp3ZGVmN3ZyIn0.3Ziuh7Utama_wz_4s8qh2g`;
-  console.log(`${htt}`)
+  console.log(`${htt}`);
 
   return (
     <IonList>
