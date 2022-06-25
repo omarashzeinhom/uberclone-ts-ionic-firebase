12204e006162ec4f9bbf7822d00c3dc950d777e4
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-25 19:50:21 +0200

    added-firebase

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+//Auth
+import {GoogleAuthProvider, getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
+const provider = new GoogleAuthProvider()
+const auth = getAuth
+
+//exported here so other files can have access to auth, provider and app
+export {app, provider, auth}
