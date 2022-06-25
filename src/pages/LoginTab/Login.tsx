9f9386e9e2ef9a18f39b8de0d9cbe24ba00296f7
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-25 18:14:06 +0200

    added-simple-gui-layout-fot-getstarted-in-login

--- src/pages/LoginTab/Login.tsx
+++ src/pages/LoginTab/Login.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonRouterLink,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <IonPage>
+      <IonContent fullscreen>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle size={"large"}>Login</IonTitle>
+          </IonToolbar>
 
-export default Login
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12">
+                <IonImg
+                  src={
+                    "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656172754/FIXITAPP/ionic-app-images/Login%20and%20get%20started/undraw_enter_uhqk_sgcssl.svg"
+                  }
+                />
+              </IonCol>
+
+              <IonCol size="12">
+                <IonButton color={"danger"} expand={"block"}>
+                  Sign in with Google
+                </IonButton>
+              </IonCol>
+
+              <IonCol size="6">
+                <IonRouterLink href="/forgotpassword">
+                  Forgot Password ?
+                </IonRouterLink>
+              </IonCol>
+              <IonCol size="6">
+                <IonRouterLink href="/register">Register account</IonRouterLink>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonHeader>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
