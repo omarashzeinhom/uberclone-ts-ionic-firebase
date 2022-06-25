624819a7eaed337095472fb9df82c3dd08741573
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-25 07:56:34 +0200

    added-calculating-prices-by-distance-

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -38,7 +38,7 @@ const Map: React.FC<MapsProps> = (props, { name }) => {
     if (props.pickupCoordinates && props.dropOffCoordinates) {
       map.fitBounds([props.dropOffCoordinates, props.pickupCoordinates, {}], {
         padding: 50,
-        zoom: 5
+        zoom: 10,
       });
     }
   }, [props.pickupCoordinates, props.dropOffCoordinates]);
